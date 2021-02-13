659b7f6ad6bc5ff4bf141bc3ca7e689c2a7fbd16
Priyanshi Gupta <48204914+Priyanshiguptaaa@users.noreply.github.com>
2021-02-14 00:19:11 +0530

    Create DetectLoopLL.cpp

---
+++ Linked_List/DetectLoopLL.cpp
@@ -0,0 +1,29 @@
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+
+bool detectLoop(Node* head)
+{
+    // your code here
+    if(head==NULL || head->next==NULL)
+        return false;
+    
+    Node *fast=head;
+    Node *slow=head;
+    
+    while((fast->next != NULL) && (fast->next->next != NULL))
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow)
+            return true;
+    }
+    return false;
+}
